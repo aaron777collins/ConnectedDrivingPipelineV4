4f5a6fc67b05482c028d99aad47fe7f4ce954876
aaron777collins <aaron777collins@gmail.com>
2023-06-23 19:26:39 -0400

    upgraded the file runner to only require one password entry

--- runAllMClassifierLargePipelineUserFiles.sh
+++ runAllMClassifierLargePipelineUserFiles.sh
@@ -7,6 +7,10 @@ files=$(ls MClassifierLargePipelineUser*.py)
 echo "Please enter your username:"
 read username
 
+# read password to feed into any inputs asked by ssh
+echo "Please enter your password:"
+read -s password
+
 # Usage: defaultrunnerconfig.sh <FILE> <USERNAME> [OPTIONAL: DEPENDENCY]
 # running the defaultrunnerconfig.sh script for each file with the previous file as a dependency
 # we need to keep track of the output of defaultrunnerconfig.sh and parse it to get the dependency
@@ -17,8 +21,10 @@ do
     dependency=$(echo $output | grep -oP '(?<=Submitted batch job )[0-9]*')
     # printing the dependency
     echo "Dependency: $dependency"
-    # running the defaultrunnerconfig.sh script
-    output=$(/bin/bash defaultrunnerconfig.sh $file $username $dependency)
+    # running the defaultrunnerconfig.sh script but with the previous file as a dependency
+    # also passing in the username and the dependency
+    # and feeding in the password to any ssh commands
+    output=$(echo $password | /bin/bash defaultrunnerconfig.sh $file $username $dependency)
     # printing the output of the defaultrunnerconfig.sh script
     echo $output
     # sleeping for 1 second to avoid overloading the scheduler
