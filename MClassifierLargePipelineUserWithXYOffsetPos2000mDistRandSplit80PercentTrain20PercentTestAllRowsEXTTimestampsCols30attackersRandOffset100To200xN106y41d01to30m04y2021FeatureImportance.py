ee7bc963f6a7a0cf147b94e921e8f8067b89bca7
aaron777collins <aaron777collins@gmail.com>
2023-07-12 13:07:25 -0400

    fixed feature importance

--- MClassifierLargePipelineUserWithXYOffsetPos2000mDistRandSplit80PercentTrain20PercentTestAllRowsEXTTimestampsCols30attackersRandOffset100To200xN106y41d01to30m04y2021FeatureImportance.py
+++ MClassifierLargePipelineUserWithXYOffsetPos2000mDistRandSplit80PercentTrain20PercentTestAllRowsEXTTimestampsCols30attackersRandOffset100To200xN106y41d01to30m04y2021FeatureImportance.py
@@ -299,7 +299,7 @@ class MClassifierLargePipelineUserWithXYOffsetPos2000mDistRandSplit80PercentTrai
         ax.set_title("Feature importances using permutation on full model")
         ax.set_ylabel("Mean accuracy decrease")
         fig.tight_layout()
-        figSavePath = self._mlPathProvider.get("MDataClassifier.plot_feature_importance_path") + "feature_importance.png"
+        figSavePath = self._mlPathProvider.getPathWithModelName("MDataClassifier.plot_feature_importance_path") + "feature_importance.png"
         plt.savefig(figSavePath)
         # read image and embed it
         csvWriter = self._contextprovider.get("MClassifierPipeline.csvWriter")
